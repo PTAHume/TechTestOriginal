786ece6f088f054d652538ace492ec52c49bdbd3
Patrick Hume <patricktahume@outlook.com>
2024-03-13 18:28:15 +0000

    add files that were ignored for some reason

---
+++ UserManagement.Web/Models/Logs/LogsListViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UserManagement.Web.Models.Users;
+
+namespace UserManagement.Web.Models.Logs;
+
+public class LogsListViewModel
+{
+    public List<LogsListItemViewModel> Items { get; set; } = [];
+}
+
+public class LogsListItemViewModel
+{
+    public long Id { get; set; }
+
+    public DateTime TimeStamp { get; set; }
+
+    public long UserId { get; set; }
+
+    public string Action { get; set; } = string.Empty;
+
+    public UserListItemViewModel? Details { get; set; }
+}
